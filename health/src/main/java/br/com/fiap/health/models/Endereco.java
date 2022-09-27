32791c50f4d5591671a624924fad263c560d7f70
Tomas Alric <tomas.alricc@gmail.com>
2022-09-26 22:58:06 -0300

    Criando entidade endereço

---
+++ health/src/main/java/br/com/fiap/health/models/Endereco.java
@@ -0,0 +1,30 @@
+package br.com.fiap.health.models;
+
+import lombok.Data;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity
+@Data
+public class Endereco {
+    @Id
+    @GeneratedValue
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+
+    private String logradouro;
+
+    private Long numero;
+
+    private String cep;
+
+    private String complemento;
+
+    private String cidade;
+
+    private String estado;
+}
